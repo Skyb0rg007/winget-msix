268f53f7d82cf12c1d459f02325a917e5e938de0
Skye Soss <skyler.soss@gmail.com>
2025-02-14 23:11:38 -0600

    Embed the installer uri in the template file

--- scripts/Build-Msix.ps1
+++ scripts/Build-Msix.ps1
@@ -2,7 +2,6 @@ Param (
     [Parameter(Mandatory)]
     [string]$TemplatePath,
 
-    [Parameter(Mandatory)]
     [string]$InstallerPath,
 
     [Parameter(Mandatory)]
@@ -14,24 +13,47 @@ $ErrorActionPreference = "Stop"
 if (-not (Test-Path $TemplatePath)) {
     throw "Invalid TemplatePath"
 }
-if (-not (Test-Path $InstallerPath)) {
-    throw "Invalid InstallerPath"
-}
 if (-not (Test-Path $OutputPath)) {
     throw "Invalid OutputPath"
+} else {
+    $OutputPath = (Resolve-Path -LiteralPath $OutputPath).Path
 }
 
 [xml]$Template = Get-Content $TemplatePath
 $PackageName = $Template.MsixPackagingToolTemplate.PackageInformation.PackageName
+
+if ($Template.MsixPackagingToolTemplate.Installer.Uri) {
+    $Uri = $Template.MsixPackagingToolTemplate.Installer.Uri
+    $Sha = $Template.MsixPackagingToolTemplate.Installer.Sha256
+    $Ext = $Template.MsixPackagingToolTemplate.Installer.Extension
+    $Template.MsixPackagingToolTemplate.Installer.RemoveAttribute("Uri")
+    $Template.MsixPackagingToolTemplate.Installer.RemoveAttribute("Sha256")
+    $Template.MsixPackagingToolTemplate.Installer.RemoveAttribute("Extension")
+
+    $InstallerPath = New-TemporaryFile | Rename-Item -NewName { [IO.Path]::ChangeExtension($_, ".$Ext") } -PassThru
+    $null = Register-EngineEvent PowerShell.Exiting -Action {
+        Remove-Item -Path $InstallerPath -ErrorAction SilentlyContinue
+    }
+    Invoke-WebRequest -Uri $Uri -OutFile $InstallerPath
+    $Hash = (Get-FileHash $InstallerPath).Hash
+    if ($Hash -ne $Sha) {
+        throw "Invalid uri hash: Expected $Sha, Got $Hash"
+    }
+} elseif (-not (Test-Path $InstallerPath)) {
+    throw "Missing InstallerPath"
+}
+
 $Template.MsixPackagingToolTemplate.Installer.Path = "$InstallerPath"
 $Template.MsixPackagingToolTemplate.SaveLocation.PackagePath = "$OutputPath\$PackageName.msix"
 $Template.MsixPackagingToolTemplate.SaveLocation.TemplatePath = "$OutputPath\${PackageName}_template.yaml"
 
-$OutputTemplate = New-TemporaryFile
-Register-EngineEvent PowerShell.Exiting -Action {
+$OutputTemplate = New-TemporaryFile | Rename-Item -NewName { [IO.Path]::ChangeExtension($_, ".xml") } -PassThru
+$null = Register-EngineEvent PowerShell.Exiting -Action {
     Remove-Item -Path $OutputTemplate -ErrorAction SilentlyContinue
 }
 
 $Template.Save($OutputTemplate)
+
+Get-Content $OutputTemplate | Write-Output
 
 MsixPackagingTool create-package --template $OutputTemplate
